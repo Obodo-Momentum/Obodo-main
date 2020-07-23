1dee511d1134f05a237602f2089282cc8a499c93
Alyshia Lozano Smith <alyshiapolk@gmail.com>
2020-07-23 19:54:01 -0400

    layout set, still working with images

---
+++ static/js/add-request-or-offer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Preview from './components/Preview'
+
+function getDataFromForm (form) {
+  const formData = new FormData(form)
+  const fields = {}
+  for (const key of formData.keys()) {
+    fields[key] = formData.get(key)
+  }
+  return fields
+}
+
+const preview = document.getElementById('preview')
+if (preview) {
+  const postForm = document.getElementById('post-form')
+  postForm.addEventListener('input', function (event) {
+    const fields = getDataFromForm(postForm)
+    ReactDOM.render(<Preview {...fields} />, preview)
+  })
+}
