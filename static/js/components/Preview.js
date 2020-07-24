38c0120cd5ce3bc4abec1b34bf22b1595b81360d
Alyshia Lozano Smith <alyshiapolk@gmail.com>
2020-07-23 20:08:41 -0400

    minor changes

--- static/js/components/Preview.js
+++ static/js/components/Preview.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 
-export default function Preview ({ title, post_text, category, request_or_offer, location, timeline_start, timeline_end, post_image, tag_names }) {
+export default function Preview ({ title, post_text, category, request_or_offer, location, timeline_start, timeline_end, post_image, tag_names  }) {
   return (
     <div>
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant='top' src='holder.js/100px180' />
+        <Card.Img variant='top' src={post_image} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Subtitle>{request_or_offer}</Card.Subtitle>
