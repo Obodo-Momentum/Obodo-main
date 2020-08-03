8b8ce67d469b09c6a5d0f48a57a62257cbb81fcb
Alyshia Lozano Smith <alyshiapolk@gmail.com>
2020-08-03 13:35:52 -0400

    minor changes

--- static/js/comment-section.js
+++ static/js/comment-section.js
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Comments from './components/comments'
 
-export function getPostPk(){
-    
-}
-
 const commentSection = document.getElementById('comment-section')
 if (commentSection) {
   ReactDOM.render(<Comments />, commentSection)
